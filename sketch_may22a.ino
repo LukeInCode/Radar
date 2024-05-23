2b3ba6f43ed9d32767cb534c083460850d6e4f33
Davide Bianchin <129370552+blondones@users.noreply.github.com>
2024-05-23 13:19:52 +0200

    Add files via upload
    Codice per Arduino con Shield, per "Sensore ad Infrarossi" e "Servo".

---
+++ sketch_may22a.ino
@@ -0,0 +1,47 @@
+#include <Servo.h>
+
+Servo myServo; // Crea un oggetto servo per controllare il servo motore
+
+int pos = 0;    // Variabile per memorizzare la posizione del servo
+int increment = 1; // Variabile per controllare la direzione del movimento
+
+void setup() {
+  Serial.begin(9600);
+  myServo.attach(11); // Collega il servo al pin 9 (puoi cambiare il pin secondo il tuo setup)
+}
+
+void loop() {
+  // Radar
+
+
+    // Ruota il servo motore da 0 a 180 gradi
+  for (int i = 0; i <= 180; i++) {
+    myServo.write(i);
+      uint16_t distance = analogRead(A2);
+    uint16_t range = function(distance);
+    Serial.print("Range: ");
+    Serial.print(range);
+    Serial.println(" mm");
+    //delay(50);
+  }
+
+  // Ruota il servo motore da 180 a 0 gradi
+  for (int i = 180; i >= 0; i--) {
+    myServo.write(i);
+    uint16_t distance = analogRead(A2);
+    uint16_t range = function(distance);
+    Serial.print("Range: ");
+    Serial.print(range);
+    Serial.println(" mm");
+    //delay(50);
+  }
+   delay(50); // Attendi 15 millisecondi per permettere al servo di raggiungere la posizione
+
+}
+
+uint16_t function(uint16_t value){
+  if(value < 10){
+    value = 10;
+  }
+  return ((67870.0 / (value - 3.0)) - 40.0);
+}
