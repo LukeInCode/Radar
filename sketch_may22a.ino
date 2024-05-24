2d3a39e36044383607a6270d20c3664391e77c0b
Luke <157395486+Lucavvi@users.noreply.github.com>
2024-05-24 11:00:41 +0200

    Update sketch_may22a.ino

--- sketch_may22a.ino
+++ sketch_may22a.ino
@@ -4,21 +4,22 @@ Servo myServo; // Crea un oggetto servo per controllare il servo motore
 
 int pos = 0;    // Variabile per memorizzare la posizione del servo
 int increment = 1; // Variabile per controllare la direzione del movimento
+const int minutes = 600000;
+uint16_t range;
+int previousMillis;
 
 void setup() {
   Serial.begin(9600);
   myServo.attach(11); // Collega il servo al pin 9 (puoi cambiare il pin secondo il tuo setup)
+  previousMillis = millis();
 }
 
 void loop() {
-  // Radar
-
-
     // Ruota il servo motore da 0 a 180 gradi
   for (int i = 0; i <= 180; i++) {
     myServo.write(i);
-      uint16_t distance = analogRead(A2);
-    uint16_t range = function(distance);
+    uint16_t distance = analogRead(A2);
+    range = toMillimetres(distance);
     Serial.print("Range: ");
     Serial.print(range);
     Serial.println(" mm");
@@ -29,17 +30,24 @@ void loop() {
   for (int i = 180; i >= 0; i--) {
     myServo.write(i);
     uint16_t distance = analogRead(A2);
-    uint16_t range = function(distance);
+    range = toMillimetres(distance);
     Serial.print("Range: ");
     Serial.print(range);
     Serial.println(" mm");
+    //controllo minuti.
     //delay(50);
   }
-   delay(50); // Attendi 15 millisecondi per permettere al servo di raggiungere la posizione
+   if(millis() - previousMillis > minutes) {
+    Serial.write(range);
+    previousMillis = millis();    
+  }
+  delay(50); // Attendi 15 millisecondi per permettere al servo di raggiungere la posizione
+
+
 
 }
 
-uint16_t function(uint16_t value){
+uint16_t toMillimetres(uint16_t value){
   if(value < 10){
     value = 10;
   }
